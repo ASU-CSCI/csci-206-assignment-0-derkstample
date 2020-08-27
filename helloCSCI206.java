9a590246ad7d3d0405161b4cd3a67a498e4fe2b7
Derek <derkikrodriguez@gmail.com>
2020-08-27 16:40:00 -0600

    added more stuff lol

--- helloCSCI206.java
+++ helloCSCI206.java
@@ -8,5 +8,7 @@ public class helloCSCI206 {
     public static void main(String[] args) {
 
         System.out.println("derek says hello");
+        System.out.println("derek says hello twice");
+        System.out.println("derek says hello thrice now, goodness");
     }
 }
